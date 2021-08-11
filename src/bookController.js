da6663e903e096cc792c7168b6a9a0f6d7154003
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-11 09:32:55 +0200

    remove duplication

--- src/bookController.js
+++ src/bookController.js
@@ -1,23 +1,32 @@
-module.exports = ({bookService, bookRepository}) => ({
-    async createOrUpdate(req, res, next) {
-        // HTTP
-        const {title, authors, isbn, description} = req.body;
+const mapValues = require("lodash.mapvalues");
+
+// const mapValues = (api, f) => Object.fromEntries(Object.entries(api).map(([key, value]) => [key, f(value)]));
+
+
+function withErrorHandling(api) {
+    return mapValues(api, wrapWithTryCatch);
+}
+
+const wrapWithTryCatch = (fn) => {
+    return async function (req, res, next) {
         try {
-            // JS
-            await bookService.createOrUpdate({title, authors, isbn, description});
-            // HTTP
-            res.redirect(`/book/${isbn}`);
-        } catch (e) {
-            next(e);
-        }
-    },
-    async details(req, res, next) {
-        try {
-            const isbn = req.params.isbn;
-            const book = await bookRepository.findOne(isbn);
-            res.json(book);
+            return fn(req, res, next);
         } catch(e) {
             next(e);
         }
+    };
+};
+
+
+module.exports = ({bookService, bookRepository}) => withErrorHandling({
+    async createOrUpdate(req, res, next) {
+        const {title, authors, isbn, description} = req.body;
+        await bookService.createOrUpdate({title, authors, isbn, description});
+        res.redirect(`/book/${isbn}`);
+    },
+    async details(req, res, next) {
+        const isbn = req.params.isbn;
+        const book = await bookRepository.findOne(isbn);
+        res.json(book);
     }
 });
