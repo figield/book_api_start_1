558413f288835197a65ab0df8562ecb050c854d1
kwasniew <kwasniewski.mateusz@gmail.com>
2021-08-11 07:55:09 +0200

    test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const nt_recursive = () => process.nextTick(nt_recursive);
 nt_recursive();
 
-// const si_recursive = () => setImmediate(si_recursive);
+const si_recursive = () => setImmediate(si_recursive);
 // si_recursive();
 
 setInterval(() => console.log('hi'), 10); // simulates other code
---
+++ test/unit/bookControllerTest.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const bookController = require('../../src/bookController');
+
+describe('Book controller', function () {
+    it('create or update happy path', async function () {
+
+    });
+});
